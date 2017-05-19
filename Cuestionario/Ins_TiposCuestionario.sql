c0ef642a27e1ee70b6acc6a4109ecdc9c11fe129
pvdasko <pvdasko@hotmail.com>
2017-05-19 18:46:02 -0500

    carga 4

---
+++ Cuestionario/Ins_TiposCuestionario.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[Ins_TiposCuestionario]
+	@Corporativo  bigint,
+	@Hotel nvarchar(10),
+	@TipoCuestionario nvarchar(5),
+	@Descripcion nvarchar (50),
+	@DescripcionIngles nvarchar (50)
+AS
+	INSERT INTO [dbo].[C_Tipos Cuestionario] VALUES (
+	@Corporativo,
+	@Hotel,
+	@TipoCuestionario,
+	@Descripcion,
+	@DescripcionIngles
+
+	)
+RETURN 0
---
+++ Cuestionario/Upd_TiposCuestionarios.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[Upd_TiposCuestionario]
+	@Corporativo  bigint,
+	@Hotel nvarchar(10),
+	@TipoCuestionario nvarchar(5),
+	@Descripcion nvarchar (50),
+	@DescripcionIngles nvarchar (50)
+AS
+	UPDATE [dbo].[C_Tipos Cuestionario] 	
+	SET Descripcion = @Descripcion,
+	[Descripcion Ingles] = @DescripcionIngles
+	WHERE Corporativo = @Corporativo
+	AND Hotel = @Hotel
+	AND [Tipo Cuestionario]=@TipoCuestionario
+RETURN 0
