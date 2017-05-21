b968610c8a86ed00bbb50d97a063ba8c7c72c483
pvdasko <pvdasko@hotmail.com>
2017-05-21 17:47:30 -0500

    Carga Solucion Cuestionario App 20/0/17

---
+++ Solucion_Cuestionario/Cuestionario.BusinessRuleComponent/Preguntas.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cuestionario.DataAccesComponent;
+
+namespace Cuestionario.BusinessRuleComponent
+{
+    public class Preguntas
+    {
+
+        #region vars&const
+        private string _errorMessage;
+        #endregion
+
+        public DataTable getTipoPreguntas()
+        {
+            DataAccess dao = new DataAccess();
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+            ds = dao.ExecuteDataSet("uSP_TipoDeclaracionConsulta");
+            if (ds.Tables.Count > 0)
+                dt = ds.Tables[0];
+
+            return dt;
+        }
+
+        public bool agregaPregunta(int DeclaracionID, string Nombre, string Usuario, DateTime Periodo)
+        {
+            bool returnValue = false;
+
+            DataAccess dao = new DataAccess();
+            SqlParameter[] parameters = new SqlParameter[4];
+            parameters[0] = new SqlParameter("@DeclaracionID", DeclaracionID);
+            parameters[1] = new SqlParameter("@Nombre", Nombre);
+            parameters[2] = new SqlParameter("@Usuario", Usuario);
+            parameters[3] = new SqlParameter("@Periodo", Periodo);
+
+            dao.ExecuteNonQuery("uSP_InsertaConceptoDeclaracion", parameters);
+            try
+            {
+                returnValue = true;
+
+            }
+            catch (Exception error)
+            {
+                _errorMessage = error.Message;
+                returnValue = false;
+
+            }
+            return returnValue;
+
+        }
+
+        public bool eliminaPregunta(int DeclaracionID, int ConceptoID)
+        {
+            bool returnValue = false;
+
+            DataAccess dao = new DataAccess();
+            SqlParameter[] parameters = new SqlParameter[2];
+            parameters[0] = new SqlParameter("@DeclaracionID", DeclaracionID);
+            parameters[1] = new SqlParameter("@ConceptoID", ConceptoID);
+            dao.ExecuteNonQuery("uSP_EliminaConceptoDeclaracion", parameters);
+            try
+            {
+                returnValue = true;
+
+            }
+            catch (Exception error)
+            {
+                _errorMessage = error.Message;
+                returnValue = false;
+
+            }
+            return returnValue;
+
+        }
+
+    }
+}
---
+++ Solucion_Cuestionario/Cuestionario.DataAccesComponent/DataAccess.cs
@@ -0,0 +1,367 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace Cuestionario.DataAccesComponent
+{
+        public class DataAccess
+    {
+        private string _connectionString;
+        private string _errorMessage;
+        private int _timeOut;
+
+        public DataAccess()
+        {
+            _connectionString = ConfigurationManager.ConnectionStrings["pagosProvisionalesConnectionString"].ConnectionString;
+            _timeOut = int.Parse(ConfigurationManager.AppSettings["CommandTimeOut"]);
+        }
+
+
+        public DataSet ExecuteDataSet(string procedureName)
+        {
+            _errorMessage = "";
+            SqlConnection conn = null;
+            DataSet ds = new DataSet();
+            try
+            {
+                conn = new SqlConnection(_connectionString);
+                SqlCommand sqlCommand = new SqlCommand(procedureName, conn);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.CommandTimeout = _timeOut;
+
+                SqlDataAdapter da = new SqlDataAdapter(sqlCommand);
+                conn.Open();
+                da.Fill(ds);
+            }
+            catch (Exception e)
+            {
+                _errorMessage = e.Message;
+            }
+            finally
+            {
+                if (conn != null && conn.State == ConnectionState.Open)
+                    conn.Close();
+            }
+            return ds;
+        }
+        public DataSet ExecuteDataSet(string procedureName, string tableName)
+        {
+            _errorMessage = "";
+            SqlConnection conn = null;
+            DataSet ds = new DataSet();
+            try
+            {
+                conn = new SqlConnection(_connectionString);
+                SqlCommand sqlCommand = new SqlCommand(procedureName, conn);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.CommandTimeout = _timeOut;
+
+                SqlDataAdapter da = new SqlDataAdapter(sqlCommand);
+                conn.Open();
+                da.Fill(ds, tableName);
+            }
+            catch (Exception e)
+            {
+                _errorMessage = e.Message;
+            }
+            finally
+            {
+                if (conn != null && conn.State == ConnectionState.Open)
+                    conn.Close();
+            }
+            return ds;
+        }
+        public DataSet ExecuteDataSet(string procedureName, SqlParameter[] parameterArray)
+        {
+            _errorMessage = "";
+            SqlConnection conn = null;
+            DataSet ds = new DataSet();
+            try
+            {
+                conn = new SqlConnection(_connectionString);
+                SqlCommand sqlCommand = new SqlCommand(procedureName, conn);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.CommandTimeout = _timeOut;
+
+                SqlParameter sqlParameter;
+
+                for (int i = 0; i < parameterArray.Length; i++)
+                {
+                    sqlParameter = sqlCommand.Parameters.Add(parameterArray[i].ParameterName.ToString(), parameterArray[i].Value.GetType());
+                    sqlParameter.Value = parameterArray[i].Value;
+
+                }
+
+                SqlDataAdapter da = new SqlDataAdapter(sqlCommand);
+                da.Fill(ds);
+            }
+            catch (Exception e)
+            {
+                _errorMessage = e.Message;
+            }
+            finally
+            {
+                if (conn != null && conn.State == ConnectionState.Open)
+                    conn.Close();
+            }
+            return ds;
+        }
+        public DataSet ExecuteSQLQuery(string sqlQuery)
+        {
+            _errorMessage = "";
+            SqlConnection conn = null;
+            DataSet ds = new DataSet();
+            try
+            {
+                conn = new SqlConnection(_connectionString);
+                SqlCommand sqlCommand = new SqlCommand(sqlQuery, conn);
+                sqlCommand.CommandText = sqlQuery;
+                sqlCommand.CommandType = CommandType.Text;
+                sqlCommand.CommandTimeout = _timeOut;
+
+                SqlDataAdapter da = new SqlDataAdapter(sqlCommand);
+                conn.Open();
+                da.Fill(ds);
+            }
+            catch (Exception e)
+            {
+                _errorMessage = e.Message;
+            }
+            finally
+            {
+                if (conn != null && conn.State == ConnectionState.Open)
+                    conn.Close();
+            }
+            return ds;
+        }
+        public int ExecuteScalar(string procedureName)
+        {
+            _errorMessage = "";
+            SqlConnection conn = null;
+            int result = -1;
+            try
+            {
+                conn = new SqlConnection(_connectionString);
+                SqlCommand sqlCommand = new SqlCommand(procedureName, conn);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.CommandTimeout = _timeOut;
+
+                conn.Open();
+                result = (int)sqlCommand.ExecuteScalar();
+            }
+            catch (Exception e)
+            {
+                _errorMessage = e.Message;
+            }
+            finally
+            {
+                if (conn != null && conn.State == ConnectionState.Open)
+                    conn.Close();
+            }
+            return result;
+        }
+        public int ExecuteScalar(string procedureName, SqlParameter[] parameterArray)
+        {
+            _errorMessage = "";
+            SqlConnection conn = null;
+            int result = -1;
+            try
+            {
+                conn = new SqlConnection(_connectionString);
+                SqlCommand sqlCommand = new SqlCommand(procedureName, conn);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.CommandTimeout = _timeOut;
+
+                SqlParameter sqlParameter;
+
+                for (int i = 0; i < parameterArray.Length; i++)
+                {
+                    sqlParameter = sqlCommand.Parameters.Add(parameterArray[i].ParameterName.ToString(), parameterArray[i].Value.GetType());
+                    sqlParameter.Value = parameterArray[i].Value;
+
+                }
+
+                conn.Open();
+                result = (int)sqlCommand.ExecuteScalar();
+            }
+            catch (Exception e)
+            {
+                _errorMessage = e.Message;
+            }
+            finally
+            {
+                if (conn != null && conn.State == ConnectionState.Open)
+                    conn.Close();
+            }
+            return result;
+        }
+        public int ExecuteNonQuery(string procedureName, SqlParameter[] parameterArray, SqlParameter returnParameter)
+        {
+            _errorMessage = "";
+            SqlConnection conn = null;
+            int result = -1;
+            try
+            {
+                conn = new SqlConnection(_connectionString);
+                SqlCommand sqlCommand = new SqlCommand(procedureName, conn);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.CommandTimeout = _timeOut;
+
+                SqlParameter sqlParameter;
+
+                for (int i = 0; i < parameterArray.Length; i++)
+                {
+                    sqlParameter = sqlCommand.Parameters.Add(parameterArray[i].ParameterName.ToString(), parameterArray[i].Value.GetType());
+                    sqlParameter.Value = parameterArray[i].Value;
+
+                }
+                sqlCommand.Parameters.Add(returnParameter);
+                conn.Open();
+                result = sqlCommand.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                _errorMessage = e.Message;
+            }
+            finally
+            {
+                if (conn != null && conn.State == ConnectionState.Open)
+                    conn.Close();
+            }
+            return result;
+        }
+        public int ExecuteNonQuery(string procedureName, SqlParameter[] parameterArray)
+        {
+            _errorMessage = "";
+            SqlConnection conn = null;
+            int result = -1;
+            try
+            {
+                conn = new SqlConnection(_connectionString);
+                SqlCommand sqlCommand = new SqlCommand(procedureName, conn);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.CommandTimeout = _timeOut;
+
+                SqlParameter sqlParameter;
+
+                for (int i = 0; i < parameterArray.Length; i++)
+                {
+                    sqlParameter = sqlCommand.Parameters.Add(parameterArray[i].ParameterName.ToString(), parameterArray[i].Value.GetType());
+                    sqlParameter.Value = parameterArray[i].Value;
+                }
+                conn.Open();
+                result = sqlCommand.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                _errorMessage = e.Message;
+            }
+            finally
+            {
+                if (conn != null && conn.State == ConnectionState.Open)
+                    conn.Close();
+            }
+            return result;
+        }
+        public SqlDataReader ExecuteReader(string procedureName)
+        {
+            _errorMessage = "";
+            SqlConnection conn = null;
+            SqlDataReader reader = null;
+            try
+            {
+                conn = new SqlConnection(_connectionString);
+                SqlCommand sqlCommand = new SqlCommand(procedureName, conn);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.CommandTimeout = _timeOut;
+
+                conn.Open();
+                reader = sqlCommand.ExecuteReader(CommandBehavior.CloseConnection);
+            }
+            catch (Exception e)
+            {
+                _errorMessage = e.Message;
+                if (conn != null && conn.State == ConnectionState.Open)
+                    conn.Close();
+            }
+            return reader;
+        }
+        public SqlDataReader ExecuteReader(string procedureName, SqlParameter[] parameterArray)
+        {
+            _errorMessage = "";
+            SqlConnection conn = null;
+            SqlDataReader reader = null;
+            try
+            {
+                conn = new SqlConnection(_connectionString);
+                SqlCommand sqlCommand = new SqlCommand(procedureName, conn);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.CommandTimeout = _timeOut;
+
+                SqlParameter sqlParameter;
+
+                for (int i = 0; i < parameterArray.Length; i++)
+                {
+                    sqlParameter = sqlCommand.Parameters.Add(parameterArray[i].ParameterName.ToString(), parameterArray[i].Value.GetType());
+                    sqlParameter.Value = parameterArray[i].Value;
+                }
+
+                conn.Open();
+                reader = sqlCommand.ExecuteReader(CommandBehavior.CloseConnection);
+            }
+            catch (Exception e)
+            {
+                _errorMessage = e.Message;
+                if (conn != null && conn.State == ConnectionState.Open)
+                    conn.Close();
+            }
+            return reader;
+        }
+        public SqlDataReader ExecuteSqlQueryReader(string sqlQuery)
+        {
+            _errorMessage = "";
+            SqlConnection conn = null;
+            SqlDataReader reader = null;
+            try
+            {
+                conn = new SqlConnection(_connectionString);
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.CommandType = CommandType.Text;
+                sqlCommand.CommandText = sqlQuery;
+                sqlCommand.Connection = conn;
+                sqlCommand.CommandTimeout = _timeOut;
+
+                conn.Open();
+                reader = sqlCommand.ExecuteReader(CommandBehavior.CloseConnection);
+            }
+            catch (Exception e)
+            {
+                _errorMessage = e.Message;
+                if (conn != null && conn.State == ConnectionState.Open)
+                    conn.Close();
+            }
+            return reader;
+        }
+        public int ExecuteBulkCopy(DataTable dtSource, string tableDestination)
+        {
+            _errorMessage = "";
+            SqlBulkCopy bulkCopy = null;
+            try
+            {
+                bulkCopy = new SqlBulkCopy(_connectionString, SqlBulkCopyOptions.TableLock);
+                bulkCopy.DestinationTableName = tableDestination;
+                bulkCopy.WriteToServer(dtSource);
+            }
+            catch (Exception e)
+            {
+                _errorMessage = e.Message;
+            }
+
+            return 0;
+        }
+    }
+}
